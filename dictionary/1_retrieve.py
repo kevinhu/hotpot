109c1f2435748b68a2deedcdc5174f32483dc61d
Kevin Hu <kevinhuwest@gmail.com>
2020-08-28 20:03:55 -0400

    convert to tables

--- dictionary/1_retrieve.py
+++ dictionary/1_retrieve.py
@@ -3,11 +3,6 @@ import zipfile
 from utils import download_from_url
 
 download_from_url(
-    "http://bcc.blcu.edu.cn/downloads/resources/BCC_LEX_Zh.zip",
-    "./data/raw/BCC_LEX_Zh.zip",
-    overwrite=False,
-)
-download_from_url(
     "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip",
     "./data/raw/cedict_1_0_ts_utf-8_mdbg.zip",
     overwrite=True,
@@ -17,12 +12,6 @@ download_from_url(
     "./data/raw/cjkvi_ids.txt",
     overwrite=True,
 )
-
-print("Unzipping BCC_LEX_Zh.zip... ", end="")
-
-with zipfile.ZipFile("./data/raw/BCC_LEX_Zh.zip", "r") as zip_ref:
-    zip_ref.extractall("./data/raw/BCC_LEX_Zh")
-    print("ok")
 
 print("Unzipping cedict_1_0_ts_utf-8_mdbg.zip... ", end="")
 
---
+++ dictionary/2_to_tables.py
@@ -0,0 +1,55 @@
+import config
+import pandas as pd
+import ujson
+from tqdm import tqdm
+
+print("Processing CEDICT... ", end="")
+
+cedict = pd.read_csv(
+    "./data/raw/cedict_1_0_ts_utf-8_mdbg/cedict_ts.u8",
+    sep="\t",
+    skiprows=30,
+    names=["line"],
+)
+
+cedict = cedict["line"].str.split(" ", 2, expand=True)
+cedict.columns = ["simplified", "traditional", "definition"]
+
+cedict.to_csv(f"./data/intermediate/cedict.txt", sep="\t")
+
+print("ok")
+
+print("Processing CJKVI... ", end="")
+
+cjkvi = pd.read_csv(
+    "./data/raw/cjkvi_ids.txt",
+    sep="\t",
+    skiprows=2,
+    names=["unicode", "character", "decomposition"],
+)
+
+cjkvi.to_csv(f"./data/intermediate/cjkvi.txt", sep="\t")
+
+print("ok")
+
+print("Processing translated corpus... ")
+
+zh_translation_json = []
+
+with open("./data/raw/translation2019zh/translation2019zh_train.json", "r") as f:
+
+    k = 0
+
+    for line in tqdm(f,total=):
+        line_json = ujson.loads(line)
+
+        zh_translation_json.append(line_json)
+
+zh_translations = pd.DataFrame(zh_translation_json)
+
+zh_translations.to_feather(
+    f"./data/intermediate/zh_translations.feather",
+    compression=config.FEATHER_COMPRESSION,
+    compression_level=config.FEATHER_COMPRESSION_LEVEL,
+)
+print("ok")
