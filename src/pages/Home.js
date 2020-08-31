a6c06d4d0eb71d6e067fae1f2652b489b804b2ca
Kevin Hu <kevinhuwest@gmail.com>
2020-08-31 15:42:26 -0400

    fuzzy search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
+
+import Fuse from "fuse.js";
+import words from "../assets/search_data.json";
+
+const options = {
+	includeScore: true,
+	keys: ["simplified", "short_definition", "toneless_pinyin"],
+};
+const fuse = new Fuse(words, options);
+var _ = require("lodash");
 
 const Home = () => {
 	let history = useHistory();
 
-	var [searchWord, setSearchWord] = useState("");
+	let [searchWord, setSearchWord] = useState("");
+	let [results, setResults] = useState([]);
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
@@ -12,14 +23,28 @@ const Home = () => {
 	};
 
 	const handleChange = (event) => {
+		event.persist();
 		setSearchWord(event.target.value);
+
+		let debouncedFn;
+
+		if (!debouncedFn) {
+			debouncedFn = _.debounce(() => {
+				setResults(fuse.search(searchWord, { limit: 8 }));
+			}, 250);
+		}
+		debouncedFn();
+		// setResults(fuse.search(searchWord));
 	};
+
+	console.log(results);
+
 	// general link hover style
 	const linkHover = `hover:text-blue-600 dark-hover:text-orange-500`;
 	return (
 		<div>
 			<div
-				className="w-1/3 absolute mx-auto text-center border-solid border-2 border-black p-12"
+				className="w-1/3 absolute mx-auto text-center border-solid border-2 border-black p-12 bg-white"
 				style={{
 					top: "40%",
 					left: "50%",
@@ -36,17 +61,42 @@ const Home = () => {
 					A modern Chinese-English dictionary
 				</div>
 				<div>
-				<form
-				onSubmit={handleSubmit}
-				className="chinese-serif px-6 bg-transparent outline-none w-full"
-			>
-					<input
-						className="text-lg chinese-serif p-2 bg-transparent outline-none w-full mx-auto border-solid border-2 border-black"
-						type="text"
-						placeholder="Search 189,432 words"
-						value={searchWord}
-					onChange={handleChange}
-					></input>
+					<form
+						onSubmit={handleSubmit}
+						className="chinese-serif px-6 bg-transparent outline-none w-full"
+					>
+						<div className="w-full relative">
+							<input
+								className="text-lg chinese-serif p-2 bg-transparent outline-none w-full mx-auto border-solid border-2 border-black"
+								type="text"
+								placeholder="Search 189,432 words"
+								value={searchWord}
+								onChange={handleChange}
+							></input>
+							{results.length > 0 && (
+								<div
+									className="absolute text-left bg-white border-solid border-2 border-black w-full"
+									style={{ marginTop: "-4px" }}
+								>
+									{results.map((result, index) => {
+										return (
+											<Link
+												to={`/word?word=${result["item"]["simplified"]}`}
+												className={linkHover}
+											>
+												<div className="p-2">
+													{
+														result["item"][
+															"simplified"
+														]
+													}
+												</div>
+											</Link>
+										);
+									})}
+								</div>
+							)}
+						</div>
 					</form>
 				</div>
 			</div>
