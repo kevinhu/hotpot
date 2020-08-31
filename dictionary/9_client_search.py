7ca62fe36b752eaaf14126011a3fcddca2b073e2
Kevin Hu <kevinhuwest@gmail.com>
2020-08-31 15:30:18 -0400

    full search data

--- dictionary/9_client_search.py
+++ dictionary/9_client_search.py
@@ -4,14 +4,24 @@ import ujson
 
 cedict = pd.read_csv(f"./data/intermediate/cedict.txt", sep="\t", index_col=0)
 
-simplified_words = list(cedict["simplified"])
-pinyin = list(cedict["pinyin"])
+bcc_lex = pd.read_csv(f"./data/intermediate/bcc_lex.txt", sep="\t", index_col=0)
+bcc_lex = bcc_lex.set_index("word", drop=False)
+cedict = cedict.join(bcc_lex, on="simplified").fillna(-1)
+cedict = cedict.drop("frequency", axis=1)
 
-with open("../src/assets/simplified_words.json", "w") as f:
-    f.write(ujson.dumps(simplified_words))
+cedict["rank"][cedict["rank"] == -1] = max(cedict["rank"])
+cedict["rank"] = cedict["rank"].astype(int)
 
-with open("../src/assets/pinyin.json", "w") as f:
-    f.write(ujson.dumps(pinyin))
+cedict["toneless_pinyin"] = [
+    "".join([y[:-1] for y in x.split(" ")]) for x in cedict["pinyin"]
+]
+cedict["short_definition"] = cedict["definition"].apply(lambda x: x[:32])
 
-# cedict["simplified"].to_json("../src/assets/simplified_words.json")
-# cedict["pinyin"].to_json("../src/assets/pinyin.json")
+search_data = cedict[
+    ["toneless_pinyin", "simplified", "traditional", "short_definition", "rank",]
+].to_dict(orient="index")
+
+search_data = list(search_data.values())
+
+with open("../src/assets/search_data.json", "w") as f:
+    f.write(ujson.dumps(search_data))
