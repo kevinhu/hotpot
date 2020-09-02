1329b7b2cbfbee316d5c1c900698c03181c8b811
Kevin Hu <kevinhuwest@gmail.com>
2020-09-01 21:36:41 -0400

    comments for 4_segment_examples

--- dictionary/4_segment_examples.py
+++ dictionary/4_segment_examples.py
@@ -10,21 +10,25 @@ tqdm.pandas()
 # Segment translated examples
 # ===========================
 
+# use downloaded segmenting index
 jieba.set_dictionary("./data/raw/dict.txt.big.txt")
 
 zh_translations = pd.read_feather(
     "./data/intermediate/zh_translations_filtered.feather",
 )
 
-
+# use space delimiter to store segmented list as string
+# (works because all sentence with English spaces were eliminated)
 def segment(sentence):
     return " ".join(jieba.cut_for_search(sentence, HMM=True))
 
 
+# process simplified sentences
 zh_translations["simplified_segmented"] = zh_translations["simplified"].progress_apply(
     segment
 )
 
+# process traditional sentences
 zh_translations["traditional_segmented"] = zh_translations[
     "traditional"
 ].progress_apply(segment)
