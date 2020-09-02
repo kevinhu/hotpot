351f7de1e3b7db60ba28f44e3cc4388d8488fb07
Kevin Hu <kevinhuwest@gmail.com>
2020-09-01 22:39:44 -0400

    comments for 9_client_search

--- dictionary/9_client_search.py
+++ dictionary/9_client_search.py
@@ -7,21 +7,27 @@ import ujson
 # Prepare a small dictionary for client-side search
 # =================================================
 
+# truncate descriptions to save space
 MAX_DESCRIPTION_LENGTH = 64
 
 cedict = pd.read_csv(f"./data/intermediate/cedict.txt", sep="\t", index_col=0)
 
+# unify CEDICT and BCC_LEX
 bcc_lex = pd.read_csv(f"./data/intermediate/bcc_lex.txt", sep="\t", index_col=0)
 bcc_lex = bcc_lex.set_index("word", drop=False)
 cedict = cedict.join(bcc_lex, on="simplified").fillna(-1)
 cedict = cedict.drop("frequency", axis=1)
 
+# use max-rank to impute missing ranks
 cedict["rank"][cedict["rank"] == -1] = max(cedict["rank"])
 cedict["rank"] = cedict["rank"].astype(int)
 
+# toneless pinyin for search
 cedict["toneless_pinyin"] = [
     "".join([y[:-1] for y in x.split(" ")]) for x in cedict["pinyin"]
 ]
+
+# truncate definitions
 cedict["short_definition"] = cedict["definition"].apply(
     lambda x: x[:MAX_DESCRIPTION_LENGTH]
 )
