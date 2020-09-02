a08adcf464a46c3ad5c05628b60b0d797beb437b
Kevin Hu <kevinhuwest@gmail.com>
2020-09-01 21:42:27 -0400

    comments for 7_fasttext_similars

--- dictionary/7_fasttext_similars.py
+++ dictionary/7_fasttext_similars.py
@@ -9,6 +9,7 @@ from scipy.spatial import cKDTree
 # Use nearest-neighbor word embeddings to find suggested words
 # ============================================================
 
+# number of nearest-neighbor words to save
 NUM_NEAREST = 16
 
 # python reduce_model.py ./data/raw/cc.zh.300.bin 100
@@ -16,14 +17,19 @@ zh_model = fasttext.load_model("./data/raw/cc.zh.100.bin")
 
 cedict = pd.read_csv(f"./data/intermediate/cedict.txt", sep="\t", index_col=0)
 
+# unify simplified+traditional words
 words = list(set(cedict["simplified"]) | set(cedict["traditional"]))
 
+# compute embeddings
 embeddings = np.array(list(map(zh_model.get_word_vector, words)))
 
+# construct K-D tree for fast queries
 kdtree = cKDTree(embeddings, leafsize=NUM_NEAREST * 2)
 
+# execute queries
 dists, nearest_indices = kdtree.query(embeddings, k=1 + NUM_NEAREST, p=2, n_jobs=6)
 
+# convert embedding distances and nearest-words to dataframe
 embeddings_dists = pd.DataFrame(dists, index=words)
 embeddings_nearest = pd.DataFrame(nearest_indices, index=words)
 embeddings_nearest = embeddings_nearest.applymap(lambda x: words[x])
