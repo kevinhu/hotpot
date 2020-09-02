2eba26ad70e25f1af615f3f0d71e45e0e163f5cb
Kevin Hu <kevinhuwest@gmail.com>
2020-09-01 21:16:40 -0400

    comments for 1_retrieve

--- dictionary/1_retrieve.py
+++ dictionary/1_retrieve.py
@@ -7,36 +7,42 @@ from utils import download_from_url
 # Download and unzip all raw files
 # =================================
 
-download_from_url(
-    "https://www.plecoforums.com/download/blogs_wordfreq-release_utf-8-txt.2602/",
-    "./data/raw/blogs_wordfreq-release_utf-8.txt",
-    overwrite=False,
-)
-
+# Word frequency calculations from Beijing Language and Culture University
 download_from_url(
     "http://bcc.blcu.edu.cn/downloads/resources/BCC_LEX_Zh.zip",
     "./data/raw/BCC_LEX_Zh.zip",
     overwrite=False,
 )
 
+# Word frequency calculations for blogs, converted to UTF-8
+download_from_url(
+    "https://www.plecoforums.com/download/blogs_wordfreq-release_utf-8-txt.2602/",
+    "./data/raw/blogs_wordfreq-release_utf-8.txt",
+    overwrite=False,
+)
+
+# CEDICT dictionary
 download_from_url(
     "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip",
     "./data/raw/cedict_1_0_ts_utf-8_mdbg.zip",
     overwrite=True,
 )
 
+# CJKVI character decompositions
 download_from_url(
     "https://raw.githubusercontent.com/cjkvi/cjkvi-ids/master/ids.txt",
     "./data/raw/cjkvi_ids.txt",
     overwrite=True,
 )
 
+# Word segmentation index for jieba
 download_from_url(
     "https://github.com/fxsjy/jieba/raw/master/extra_dict/dict.txt.big",
     "./data/raw/dict.txt.big.txt",
     overwrite=True,
 )
 
+# FastText CommonCrawl word vectors
 download_from_url(
     "https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.zh.300.bin.gz",
     "./data/raw/cc.zh.300.bin.gz",
