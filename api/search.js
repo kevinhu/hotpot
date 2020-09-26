41cfbae0ec0f0620ecae723fcaf5146b490ec540
Kevin Hu <kevinhuwest@gmail.com>
2020-09-26 11:48:04 -0400

    Use min flexsearch

--- api/search.js
+++ api/search.js
@@ -1,9 +1,6 @@
-var FlexSearch = require("flexsearch");
-fs = require("fs");
+var FlexSearch = require("./flexsearch.min.js");
 
 var index = new FlexSearch({
-	// default values:
-
 	encode: "icase",
 	threshold: 8,
 	resolution: 9,
@@ -14,11 +11,11 @@ var index = new FlexSearch({
 	doc: {
 		id: "id",
 		field: [
-			"toneless_pinyin",
-			"short_definition",
+			"definition",
 			"simplified",
 			"traditional",
 			"pinyin",
+			"toneless_pinyin",
 		],
 	},
 });
@@ -28,29 +25,11 @@ index.import(require("./index.json"), { serialize: false });
 exports.handler = async (event, context) => {
 	const query = event.queryStringParameters.query;
 
-	var results = index.search(query, {
+	const results = index.search(query, {
 		limit: 10,
 		// threshold: 5,
 		// depth: 3,
 	});
-
-	console.log(index.info());
-
-	// let fuzzyResults = fuzzysort.go(query, SearchData, {
-	// 	keys: [
-	// 		"toneless_pinyin",
-	// 		"short_definition",
-	// 		"simplified",
-	// 		"traditional",
-	// 		"pinyin",
-	// 	],
-	// 	allowTypo: false,
-	// 	limit: 8,
-	// 	threshold: -100,
-	// });
-	// fuzzyResults = fuzzyResults.sort((a, b) =>
-	// 	a.obj.rank >= b.obj.rank ? 1 : -1
-	// );
 
 	return {
 		statusCode: 200,
