16a94216a7de29c4adac1406c62f728868da8268
Kevin Hu <kevinhuwest@gmail.com>
2020-10-05 01:21:59 -0400

    Update search_.js

--- api/search_.js
+++ api/search_.js
@@ -1,28 +1,8 @@
 const { index } = require("./flexsearch/settings.js");
 
-var fs = require("fs");
-
-const BSON = require("bson");
-
-const simdjson = require("simdjson");
-
 console.time("index_load");
 const indexFile = require("./index.json");
-
-// const indexFile = fs.readFileSync("./index.json");
-
-// const indexBuffer = fs.readFileSync("./index.json");
 console.timeEnd("index_load");
-
-// console.time("index_parse");
-// const indexFile = JSON.parse(indexBuffer);
-
-// const indexFile = simdjson.lazyParse(indexBuffer);
-
-// var indexFile = new FastJson();
-// indexFile.write(indexBuffer);
-// console.log(indexFile[0]);
-// console.timeEnd("index_parse");
 
 console.time("index_import");
 index.import(indexFile, { serialize: false });
